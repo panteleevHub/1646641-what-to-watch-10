c72be60e6dd32a45c66885b5a22b2b948687ac5f
Victor Panteleev <panteleevelo357@gmail.com>
2022-07-11 12:16:17 +0300

    Создает компоненты страниц приложения

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type FilmPreviewProps = {
+  title: string,
+  genre: string,
+  releaseDate: number,
+}
+
+function App({title, genre, releaseDate}: FilmPreviewProps): JSX.Element {
+  return (
+    <MainScreen
+      title={title}
+      genre={genre}
+      releaseDate={releaseDate}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const FilmPreview = {
+  Title: 'The Grand Budapest Hotel',
+  Genre: 'Drama',
+  ReleaseDate: 2014,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={FilmPreview.Title}
+      genre={FilmPreview.Genre}
+      releaseDate={FilmPreview.ReleaseDate}
+    />
   </React.StrictMode>,
 );
