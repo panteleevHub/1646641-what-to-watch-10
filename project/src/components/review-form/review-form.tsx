b60599f9b7653ec5fa6755db3357189674cebef2
Victor Panteleev <panteleevelo357@gmail.com>
2022-07-23 13:19:58 +0300

    Рефакторит компонент ReviewForm

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -1,57 +1,39 @@
-import {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
+
+const STARS_COUNT = 10;
 
 function ReviewForm (): JSX.Element {
   const [formData, setFormData] = useState({
     rating: '',
-    review: '',
+    'review-text': '',
   });
 
-  const handleInputChange = ({target}: ChangeEvent<HTMLInputElement>) => {
-    setFormData({...formData, rating: target.value});
+  const handleFieldChange = ({target}: ChangeEvent<(HTMLInputElement | HTMLTextAreaElement)>) => {
+    const {name, value} = target;
+    setFormData({...formData, [name]: value});
   };
 
-  const handleTextAreaChange = ({target}: ChangeEvent<HTMLTextAreaElement>) => {
-    setFormData({...formData, review: target.value});
-  };
+  const starButtons = Array.from({length: STARS_COUNT}, (value, index) => {
+    const key = STARS_COUNT - index;
+
+    return (
+      <React.Fragment key={key}>
+        <input onChange={handleFieldChange} className="rating__input" id={`star-${key}`} type="radio" name="rating" value={`${key}`} />
+        <label className="rating__label" htmlFor={`star-${key}`}>{`Rating ${key}`}</label>
+      </React.Fragment>
+    );
+  });
 
   return (
     <form action="#" className="add-review__form">
       <div className="rating">
         <div className="rating__stars">
-          <input onChange={handleInputChange} className="rating__input" id="star-10" type="radio" name="rating" value="10" />
-          <label className="rating__label" htmlFor="star-10">Rating 10</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-9" type="radio" name="rating" value="9" />
-          <label className="rating__label" htmlFor="star-9">Rating 9</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-8" type="radio" name="rating" value="8" defaultChecked/>
-          <label className="rating__label" htmlFor="star-8">Rating 8</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-7" type="radio" name="rating" value="7" />
-          <label className="rating__label" htmlFor="star-7">Rating 7</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-6" type="radio" name="rating" value="6" />
-          <label className="rating__label" htmlFor="star-6">Rating 6</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-5" type="radio" name="rating" value="5" />
-          <label className="rating__label" htmlFor="star-5">Rating 5</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-4" type="radio" name="rating" value="4" />
-          <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-3" type="radio" name="rating" value="3" />
-          <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-2" type="radio" name="rating" value="2" />
-          <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-          <input onChange={handleInputChange} className="rating__input" id="star-1" type="radio" name="rating" value="1" />
-          <label className="rating__label" htmlFor="star-1">Rating 1</label>
+          {starButtons}
         </div>
       </div>
 
       <div className="add-review__text">
-        <textarea onChange={handleTextAreaChange} className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+        <textarea onChange={handleFieldChange} className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
         <div className="add-review__submit">
           <button className="add-review__btn" type="submit">Post</button>
         </div>
